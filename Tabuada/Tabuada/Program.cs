31e46689d5a427167c4bc63c1dce6dbb4bdf350d
joce-line <joceline.gutierrez@hotmail.com>
2022-04-06 14:36:37 -0300

    @joce-line [Atividade Tabuada]

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada do {n}: ");
+
+            for (int x = 1; x <=10 ; x++)
+            {
+                Console.WriteLine($"{n} x {x} = {n*x}");
+              
+            }
+        }
+    }
+}
