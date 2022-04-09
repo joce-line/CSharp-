d5f869394a4b671f0bccd3d91328ae7da15039a7
joce-line <joceline.gutierrez@hotmail.com>
2022-04-08 23:25:56 -0300

    Atividade Classe

---
+++ PersonagensHP/Program.cs
@@ -0,0 +1,27 @@
+﻿using PersonagensHP.src;
+using System;
+
+namespace PersonagensHP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem harry = new Personagem("Harry Potter", "Grifinória", "cervo");
+            Personagem hermione = new Personagem("Hermione Granger", "Grifinória", "lontra");
+            Personagem rony = new Personagem("Rony Weasley", "Grifinória", "cachorro terrier");
+            Personagem severo = new Personagem("Severo Snape", "Sonserina", "corça");
+
+            Console.WriteLine($"{harry.Nome} é da casa de {harry.CasaHogwarts}, seu patrono é {harry.Patrono}.");
+            Console.WriteLine($"{hermione.Nome} é da casa de {hermione.CasaHogwarts}, seu patrono é {hermione.Patrono}.");
+            Console.WriteLine($"{rony.Nome} é da casa de {rony.CasaHogwarts}, seu patrono é {rony.Patrono}.");
+            Console.WriteLine($"{severo.Nome} é da casa de {severo.CasaHogwarts}, seu patrono é {severo.Patrono}.");
+
+            harry.Defender("Expelliarmus");
+            hermione.Atacar("Petrificus Totalus");
+            severo.Atacar("Sectumsempra");
+            rony.Mover();
+            rony.Pegar();
+        }
+    }
+}
---
+++ PersonagensHP/src/Personagem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonagensHP.src
+{
+    public class Personagem
+    {
+        public string Nome { get; set; }
+        public string CasaHogwarts { get; set; }
+        public string Patrono { get; set; }
+        
+
+        public Personagem(string _nome, string _casahogwarts, string _patrono)
+        {
+            Nome = _nome;
+            CasaHogwarts = _casahogwarts;
+            Patrono = _patrono;
+            
+        }
+        public void Defender (string defesa)
+        {
+            Console.WriteLine($"{defesa}");
+        }
+        public void Atacar (string ataque)
+        {
+            Console.WriteLine($"{ataque}!");
+        }
+        public void Mover ()
+        {
+            Console.WriteLine("Para onde ir? ");
+            string lugar = Console.ReadLine();
+            Console.WriteLine($"Vou aparatar para {lugar}...");
+        }
+        public  void Pegar()
+        {
+            Console.WriteLine("O que quer pegar?");
+            string objeto = Console.ReadLine();
+            Console.WriteLine($"Accio {objeto}!");
+        }
+        
+
+
+    }
+}
