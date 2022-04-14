912b3b743c2b81f19ea4c55abdc4b1fe244f303a
joce-line <joceline.gutierrez@hotmail.com>
2022-04-14 00:09:40 -0300

    Atividade atualizada

--- Polimorfismo/Program.cs
+++ Polimorfismo/Program.cs
@@ -12,34 +12,34 @@ namespace Polimorfismo
 
             List<Animal> animais = new List<Animal>(
                 new Animal[]{
-                    new Leao("Leao do Proerd", "Laranja", "Terrestre"),
-                    new Ave("Ave do mato", "Amarelo", "Aéreo"),
-                    new Peixe("Peixe Dori", "Vermelho", "Aquático"),
-                    new Cachorro("Cachorro Coragem", "Rosa", "Terrestre")
+                    new Leao("Leão", "Laranja", "Terrestre"),
+                    new Ave("Ave", "Amarelo", "Aéreo"),
+                    new Peixe("Peixe", "Vermelho", "Aquático"),
+                    new Cachorro("Cachorro", "Marrom", "Terrestre")
                 }
             );
 
             foreach (Animal animal in animais)
             {
-                if (animal.Nome == "Leao do Proerd")
+                if (animal.Nome == "Leão")
                 {
                     Console.WriteLine($"{animal.Nome} tem a cor {animal.Cor} e é um animal {animal.Classificacao}");
-                    animal.Comunicar("Ensinando a dizer não!");
+                    animal.Comunicar("ROAAAAAAR..");
                 }
-                else if (animal.Nome == "Ave do mato")
+                else if (animal.Nome == "Ave")
                 {
                     Console.WriteLine($"{animal.Nome} tem a cor {animal.Cor} e é um animal {animal.Classificacao}");
                     animal.Comunicar("piu-piu piopio pipio!");
                 }
-                else if (animal.Nome == "Peixe Dori")
+                else if (animal.Nome == "Peixe")
                 {
                     Console.WriteLine($"{animal.Nome} tem a cor {animal.Cor} e é um animal {animal.Classificacao}");
-                    animal.Comunicar("Continue a nadar....");
+                    animal.Comunicar("glub glub...");
                 }
-                else if (animal.Nome == "Cachorro Coragem")
+                else if (animal.Nome == "Cachorro")
                 {
                     Console.WriteLine($"{animal.Nome} tem a cor {animal.Cor} e é um animal {animal.Classificacao}");
-                    animal.Comunicar("Murieeel!");
+                    animal.Comunicar("Au au au!");
                 }
             }
             
